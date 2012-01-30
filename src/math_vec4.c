cbbee25534d7a812c5734189c2053f751212fc22
Andre Puschmann <andre.puschmann@tu-ilmenau.de>
2012-01-30 07:50:12 -0600

    uncomment unused function to fix compile issue

--- src/math_vec4.c
+++ src/math_vec4.c
@@ -112,7 +112,7 @@ dot4_neon_hfp(float v0[4], float v1[4])
 #endif
 }
 
-
+#if 0
 float32_t 
 dot4_neon(float32x4_t v0, float32x4_t v1)
 {	
@@ -127,6 +127,7 @@ dot4_neon(float32x4_t v0, float32x4_t v1)
 	r = vpadd_f32(r, r);
 	return vget_lane_f32(r, 0);
 }
+#endif
 
 float 
 dot4_neon_sfp(float v0[4], float v1[4])
